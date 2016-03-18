4461f66a44f8fda7bf8f4cdf8720f52dce8de1fe
Alfie Hanssen <alfiehanssen@gmail.com>
2016-03-18 14:37:30 -0400

    removed unused code

--- Example/VimeoUpload-iOS-2Step/VimeoUpload-iOS-2Step/Controllers/NewVimeoUpload.swift
+++ Example/VimeoUpload-iOS-2Step/VimeoUpload-iOS-2Step/Controllers/NewVimeoUpload.swift
@@ -37,9 +37,5 @@ class NewVimeoUpload: VimeoUpload<UploadDescriptor>
     override init(backgroundSessionIdentifier: String, authTokenBlock: AuthTokenBlock)
     {
         super.init(backgroundSessionIdentifier: backgroundSessionIdentifier, authTokenBlock: authTokenBlock)
-        
-        let test = VimeoUpload<UploadDescriptor>(backgroundSessionIdentifier: "") { () -> String? in
-            return nil
-        }
     }
 }
