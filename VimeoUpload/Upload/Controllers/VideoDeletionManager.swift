b4f7068199a4c1a020091dd554ac9aa8bb015ee2
Alfie Hanssen <alfiehanssen@gmail.com>
2016-04-25 14:34:24 -0400

    About to set up framework project

--- VimeoUpload/Upload/Controllers/VideoDeletionManager.swift
+++ VimeoUpload/Upload/Controllers/VideoDeletionManager.swift
@@ -26,6 +26,7 @@
 
 import Foundation
 import AFNetworking
+import VimeoNetworking
 
 public class VideoDeletionManager: NSObject
 {
--- VimeoUpload/Upload/Operations/Async/DeleteVideoOperation.swift
+++ VimeoUpload/Upload/Operations/Async/DeleteVideoOperation.swift
@@ -25,6 +25,7 @@
 //
 
 import Foundation
+import VimeoNetworking
 
 class DeleteVideoOperation: ConcurrentOperation
 {
