b4a77f7dcb4c0d3c5ace2af539894946a2b51c1c
Van Le Nguyen <van@vimeo.com>
2018-05-29 18:45:21 -0400

    Make key archiver non-optional

--- VimeoUpload/Descriptor System/ReachableDescriptorManager.swift
+++ VimeoUpload/Descriptor System/ReachableDescriptorManager.swift
@@ -64,6 +64,7 @@ import VimeoNetworking
     ///   - accessTokenProvider: A closure that provides an authenticated
     ///   token. Any upload needs this token in order to work properly.
     ///   - apiVersion: The API version to use.
+    /// - Returns: `nil` if the keyed archiver cannot load descriptors' archive.
     public init?(name: String, documentsFolderURL: URL, backgroundSessionIdentifier: String, sharedContainerIdentifier: String? = nil, descriptorManagerDelegate: DescriptorManagerDelegate? = nil, accessTokenProvider: @escaping VimeoRequestSerializer.AccessTokenProvider, apiVersion: String)
     {
         let backgroundSessionManager: VimeoSessionManager
