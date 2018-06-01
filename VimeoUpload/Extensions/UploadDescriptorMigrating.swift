849ce3d38f77f5ba36ad28b10587eb41c508bcac
Van Le Nguyen <van@vimeo.com>
2018-06-01 11:12:28 -0400

    Declare `UploadDescriptorMigrating` interface

--- VimeoUpload/Extensions/UploadDescriptorMigrating.swift
+++ VimeoUpload/Extensions/UploadDescriptorMigrating.swift
@@ -6,3 +6,13 @@
 //  Copyright © 2018 Vimeo. All rights reserved.
 //
 
+protocol UploadDescriptorMigrating
+{
+    init(appSandboxDocumentsFolderURL: URL)
+    
+    var archiveFileExists: Bool { get }
+    
+    func loadArchiveFile() -> Set<Descriptor>
+    
+    func deleteArchiveFile()
+}
