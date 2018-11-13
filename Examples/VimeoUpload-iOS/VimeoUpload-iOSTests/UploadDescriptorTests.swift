6c3b0ceafe48b2e9fc4495a882d82bf1be541549
Van Le Nguyen <van@vimeo.com>
2018-11-13 10:45:42 -0500

    Add placeholder test methods

--- Examples/VimeoUpload-iOS/VimeoUpload-iOSTests/UploadDescriptorTests.swift
+++ Examples/VimeoUpload-iOS/VimeoUpload-iOSTests/UploadDescriptorTests.swift
@@ -7,27 +7,40 @@
 //
 
 import XCTest
+import AFNetworking
+@testable import VimeoUpload
 
-class UploadDescriptorTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class UploadDescriptorTests: XCTestCase
+{
+    private var urlSessionManager: AFURLSessionManager?
+    
+    override func setUp()
+    {
+        super.setUp()
+        
+        let configuration = URLSessionConfiguration.default
+        self.urlSessionManager = AFURLSessionManager(sessionConfiguration: configuration)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    override func tearDown()
+    {
+        self.urlSessionManager?.invalidateSessionCancelingTasks(true)
+        
+        self.urlSessionManager = nil
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func test_uploadLink_returnsStreamingLink_whenStreamingLinkIsAvailable()
+    {
+        
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_uploadLink_returnsGCSLink_whenGCSLinkIsAvailable()
+    {
+        
     }
-
+    
+    func test_uploadLink_throwsError_whenNeitherStreamLinkNorGCSLinkIsAvailable()
+    {
+        
+    }
 }
