17167a334f2aa33dc362c64250c066ba584e9185
Van Le Nguyen <van@vimeo.com>
2018-12-07 15:41:03 -0500

    Add documentation

--- VimeoUpload/Upload/Extensions/Retriable.swift
+++ VimeoUpload/Upload/Extensions/Retriable.swift
@@ -8,8 +8,16 @@
 
 import Foundation
 
+/// Classes conforming to `Retriable` can determine if they should
+/// retry uploading/downloading based on the server response.
 public protocol Retriable
 {
+    /// Determines if an upload/download should retry based on an HTTP
+    /// response.
+    ///
+    /// - Parameter urlResponse: An HTTP response.
+    /// - Returns: `true` if an upload/download should retry, and `false`
+    /// otherwise.
     func shouldRetry(urlResponse: URLResponse?) -> Bool
 }
 
